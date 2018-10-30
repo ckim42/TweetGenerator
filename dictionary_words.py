dd44c97932d6cffc824f3d3303cc2a5339e6da7c
Cherish Kim <42522882+ilikecoffee18@users.noreply.github.com>
2018-10-30 10:39:26 -0700

    fixed; no longer duplicates words

--- dictionary_words.py
+++ dictionary_words.py
@@ -1,18 +1,20 @@
 import random
 import sys
 
-i = 0
-location = int(sys.argv[1])
 def load_word():
-    f = open('words.txt', 'r')
-    words_list = f.readlines()
-    f.close()
-    split = words_list = words_list[0].split(' ')
-    word = random.choice(split)
-    return word
+    words = []
+    i = 0
+    length = int(sys.argv[1])
+    while i < length:
+        f = open('words.txt', 'r').read().split()
+        word = random.choice(f)
+        words.append(word)
+        show = " ".join(words)
+        i += 1
+    return show
 
-while i < location:
-    print(load_word())
-    i += 1
+if __name__ == '__main__':
+    scattered = load_word()
+    print(scattered)
 
-#Thanks to Anwar Azeez and Erica Naglik
+#Thanks to Dylan Finn, Anwar Azeez, & Erica Naglik
